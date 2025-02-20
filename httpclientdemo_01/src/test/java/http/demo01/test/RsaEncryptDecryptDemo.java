dfd6201c769e969c4c26b3dae52e42c957eeb218
poershouche <zzzdr_zhang@163.com>
2025-02-20 11:02:25 +0800

    config init 03

---
+++ httpclientdemo_01/src/test/java/http/demo01/test/RsaEncryptDecryptDemo.java
@@ -0,0 +1,48 @@
+package http.demo01.test;
+
+import javax.crypto.Cipher;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
+
+public class RsaEncryptDecryptDemo {
+    public static String encrypt(String plainText , PublicKey publicKey) throws Exception{
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE,publicKey);
+        byte[] bytes = cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
+        return Base64.getEncoder().encodeToString(bytes);
+    }
+    public static String decrypt(String encodeText , PrivateKey privateKey) throws Exception{
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] bytes = cipher.doFinal(Base64.getDecoder().decode(encodeText.getBytes(StandardCharsets.UTF_8)));
+        return new String(bytes);
+    }
+}
+class RSAKeyGenerator{
+    private static final String ALGORITHM = "RSA";
+    public static KeyPair generatorKeyPair() throws  Exception{
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(ALGORITHM);
+        keyPairGenerator.initialize(2048);
+        return keyPairGenerator.generateKeyPair();
+    }
+}
+
+class TEST{
+    public static void main(String[] args) throws Exception{
+        KeyPair keyPair = RSAKeyGenerator.generatorKeyPair();
+        PublicKey aPublic = keyPair.getPublic();
+        PrivateKey aPrivate = keyPair.getPrivate();
+//        System.out.println("公钥："+aPublic);
+//        System.out.println("私钥："+aPrivate);
+        String plainText="wangwu";
+        String encrypt = RsaEncryptDecryptDemo.encrypt(plainText, aPublic);
+        System.out.println(encrypt);
+        Thread.sleep(1000);
+        String decrypt = RsaEncryptDecryptDemo.decrypt(encrypt, aPrivate);
+        System.out.println(decrypt);
+    }
+}
